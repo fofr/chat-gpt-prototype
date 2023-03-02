cdf30487eb5f0f1a9a00e61b0278f29221f1d701
Paul Hayes <fofr@users.noreply.github.com>
2023-03-02 21:53:06 +0000

    Add proof of concept

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,19 @@
 import express from 'express'
 import { exampleWizardRoutes } from './routes/example-wizard.js'
+import { ChatGPTAPI } from 'chatgpt'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+async function example () {
+  const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+
+  const res = await api.sendMessage('Hello World!')
+  console.log(res.text)
+}
+
+example()
 
 const router = express.Router()
 exampleWizardRoutes(router)
